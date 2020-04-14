18cc16822816aaf5f91cf23958353f683cd53f73
ave <melgajes@gmail.com>
2020-04-13 17:20:12 -0700

    specific relations seeder and interactive seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,21 +13,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-    
+        if ($this->command->confirm('Do you want to refresh the database?')) {
+            $this->command->call('migrate:refresh');
+            $this->command->info('Database was refreshed');
+        }
 
-        $basubas = factory(App\User::class)->states('melody-basubas')->create();
-        $else = factory(App\User::class, 20)->create();
-
-        $users = $else->concat([$basubas]);
-
-        $posts = factory(App\BlogPost::class, 20)->make()->each(function($post) use ($users) {
-            $post->user_id = $users->random()->id;
-            $post->save();
-        });
-
-        $comments = factory(App\Comment::class, 150)->make()->each(function ($comment) use ($posts) {
-            $comment->blog_post_id = $posts->random()->id;
-            $comment->save();
-        });
+        $this->call([
+            UsersTableSeeder::class, 
+            BlogPostsTableSeeder::class, 
+            CommentsTableSeeder::class
+        ]);
     }
 }
