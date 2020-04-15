f6750bb992e5de6f344c78daa73643fd854fd735
ave <melgajes@gmail.com>
2020-04-15 12:30:19 -0700

    verifying permission in blade templates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,24 +25,16 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        //Gate::define('update-post', function ($user, $post) {
-        //    return $user->id == $post->user_id;
-        //});
-       // Gate::define('delete-post', function ($user, $post) {
-        //    return $user->id == $post->user_id;
-        //});
-        //Gate::before(function ($user, $ability) {
-         //   if ($user->is_admin && in_array($ability, ['update-post','delete-post'])) {
-         //       return true;
-        //    }
-       // });
-
-        // Gate::after(function ($user, $ability, $result) {
-        //     if ($user->is_admin) {
-        //         return true;
-        //     }
+       // Gate::define('update-post', function ($user, $post) {
+        //     return $user->id == $post->user_id;
         // });
-        //Gate::resource('posts', 'App\Policies\BlogPostPolicy');
+        // Gate::allows('update-post', $post);
+        // $this->authorize('update-post',)
+        // Gate::define('delete-post', function ($user, $post) {
+        //     return $user->id == $post->user_id;
+        // });
+        // Gate::define('posts.update', 'App\Policies\BlogPostPolicy@update');
+        // Gate::define('posts.delete', 'App\Policies\BlogPostPolicy@delete');
         // Gate::resource('posts', 'App\Policies\BlogPostPolicy');
         // posts.create, posts.view, posts.update, posts.delete
         // comments.create, comments.update etc.
@@ -52,6 +44,12 @@ class AuthServiceProvider extends ServiceProvider
         //         return true;
         //     }
         // });
+        Gate::before(function ($user, $ability) {
+            if ($user->is_admin && in_array($ability, ['update','delete'])) {
+                return true;
+            }
+        });
+
         // Gate::after(function ($user, $ability, $result) {
         //     if ($user->is_admin) {
         //         return true;
