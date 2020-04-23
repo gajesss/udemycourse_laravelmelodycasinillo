fec1e8a71c66185e923ac2670324187b33016fbc
ave <melgajes@gmail.com>
2020-04-23 12:36:50 -0700

    Practical:image model,1to1 relationship and migration

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\BlogPost;
 use App\Http\Requests\StorePost;
 use Illuminate\Http\Request;
-
+use App\Image;
 use Illuminate\Support\Facades\Storage;
 
 use App\User;
@@ -117,25 +117,12 @@ class PostController extends Controller
         $validatedData = $request->validated();
         $validatedData['user_id'] = $request->user()->id;
         $blogPost = BlogPost::create($validatedData);
-        $hasFile = $request->hasFile('thumbnail');
-        dump($hasFile);
-
-        if ($hasFile) {
-            $file = $request->file('thumbnail');
-            dump($file);
-            dump($file->getClientMimeType());
-            dump($file->getClientOriginalExtension());
-
-            dump($file->store('thumbails'));
-            dump(Storage::disk('public')->putFile('thumbails', $file));
-
-            $name1 = $file->storeAs('thumbails', $blogPost->id . '.'. $file->guessExtension());
-            $name2 = Storage::disk('local')->putFileAs('thumbails', $file, $blogPost->id . '.' . $file->guessExtension());
-
-            dump(Storage::url($name1));
-            dump(Storage::disk('local')->url($name2));
+        if ($request->hasFile('thumbnail')) {
+            $path = $request->file('thumbnail')->store('thumbnails');
+            $blogPost->image()->save(
+                Image::create(['path' => $path])
+            );
         }
-        die;
         $request->session()->flash('status', 'Blog post was created!');
         
 
